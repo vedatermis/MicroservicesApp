d411f4a48ab8d26e80a36642bc448e9deaa91ebc
Vedat ERMİŞ <vedatermis@hotmail.com>
2021-08-10 17:35:22 +0300

    commit

---
+++ EventBusRabbitMQ/Producer/EventBusRabbitMqProducer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using EventBusRabbitMQ.Events.Interfaces;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMqProducer
+    {
+        private readonly IRabbitMqPersistentConnection _persistentConnection;
+        private readonly ILogger<EventBusRabbitMqProducer> _logger;
+        private readonly int _retryCount;
+
+        public EventBusRabbitMqProducer(IRabbitMqPersistentConnection persistentConnection, ILogger<EventBusRabbitMqProducer> logger, int retryCount = 5)
+        {
+            _persistentConnection = persistentConnection;
+            _logger = logger;
+            _retryCount = retryCount;
+        }
+
+        public void Publish(string queueName, IEvent @event)
+        {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            var policy = RetryPolicy.Handle<BrokerUnreachableException>()
+            .Or<SocketException>()
+            .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+            {
+                _logger.LogWarning(ex, "Could not publish event: {EventId} after {Timeout}s ({ExceptionMessage})", @event.RequestId, $"{time.TotalSeconds:n1}", ex.Message);
+            });
+
+            using (var channel = _persistentConnection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                policy.Execute(() =>
+                {
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    properties.DeliveryMode = 2;
+
+                    channel.ConfirmSelect();
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: queueName,
+                        mandatory: true,
+                        basicProperties: properties,
+                        body: body);
+                    channel.WaitForConfirmsOrDie();
+
+                    channel.BasicAcks += (sender, eventArgs) =>
+                    {
+                        Console.WriteLine("Sent RabbitMQ");
+                        //implement ack handle
+                    };
+                });
+            }
+        }
+    }
+}
