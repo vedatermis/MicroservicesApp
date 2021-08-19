44b373b2bb24a1008c2e8cb09d1aa4de56aea221
Vedat ERMİŞ <vedatermis@hotmail.com>
2021-08-19 17:11:03 +0300

    commit

---
+++ Esourcing.UI/Controllers/AuctionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Esourcing.UI.Controllers
+{
+    public class AuctionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
